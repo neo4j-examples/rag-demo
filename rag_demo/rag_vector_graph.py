2e3adfcb970ef42b797db5a700aef1881d940454
Jennifer Reif <jennifer@thehecklers.org>
2024-01-25 11:21:25 -0600

    Working on vector graph query - not working (#14)
    * WIP vector graph query - retrieval_query not working
    
    ---------
    
    Co-authored-by: jalakoo <jalakoo@users.noreply.github.com>

--- rag_demo/rag_vector_graph.py
+++ rag_demo/rag_vector_graph.py
@@ -13,6 +13,30 @@ from langchain.vectorstores.neo4j_vector import Neo4jVector
 from langchain.chains import RetrievalQAWithSourcesChain
 
 
+PROMPT_TEMPLATE = """Human: You are a Financial expert with SEC filings who can answer questions only based on the context below.
+* Answer the question STRICTLY based on the context provided in JSON below.
+* Do not assume or retrieve any information outside of the context 
+* Use three sentences maximum and keep the answer concise
+* Think step by step before answering.
+* Do not return helpful or extra text or apologies
+* Just return summary to the user. DO NOT start with Here is a summary
+* List the results in rich text format if there are more than one results
+* If the context is empty, just respond None
+
+<question>
+{input}
+</question>
+
+Here is the context:
+<context>
+{context}
+</context>
+
+Assistant:"""
+PROMPT = PromptTemplate(
+    input_variables=["input","context"], template=PROMPT_TEMPLATE
+)
+
 EMBEDDING_MODEL = OpenAIEmbeddings()
 
 url = st.secrets["NEO4J_URI"]
@@ -29,8 +53,13 @@ graph = Neo4jGraph(
 @retry(tries=5, delay=5)
 def get_results(question):
 
-    index_name = "document_text_openai_embeddings"
-    node_property_name = "text_openai_embedding"
+    # TODO: Update index and node property names to reflect the embedding origin LLM,
+    # ie "document_text_openai" index and "text_openai_embedding"
+    # Currently the try-except block below only works with small datasets, it needs to be replaced 
+    # with a large node count variation
+
+    index_name = "form_10k_chunks"
+    node_property_name = "textEmbedding"
     url=st.secrets["NEO4J_URI"]
     username=st.secrets["NEO4J_USERNAME"]
     password=st.secrets["NEO4J_PASSWORD"]
@@ -47,16 +76,25 @@ def get_results(question):
         + companyName + '\n' + managers as text, similarity as score, {source: document.source} AS metadata
     ORDER BY similarity ASC // so that best answers are the last
 """
+#     retrieval_query = f"""
+#     WITH node AS doc, score
+#     OPTIONAL MATCH (doc)<-[:HAS_CHUNK]-(:Form)-[:FILED]->(company:Company), (company)<-[:OWNS_STOCK_IN]-(manager:Manager)
+#     WITH doc, score, company.name as companyName, collect(manager.managerName) as managers
+#     RETURN doc.text AS text, score, {{companyName: companyName, assetManager: managers, popularityScore: doc.score, source: doc.source}} as metadata
+#     ORDER BY score DESC LIMIT 5
+# """
 
 
     try:
         store = Neo4jVector.from_existing_index(
-            EMBEDDING_MODEL,
+            embedding=EMBEDDING_MODEL,
             url=url,
             username=username,
             password=password,
+            database="neo4j",
             index_name=index_name,
-            retrieval_query= retrieval_query,
+            embedding_node_property=node_property_name,
+            retrieval_query=retrieval_query,
         )
     except:
         store = Neo4jVector.from_existing_graph(
@@ -68,17 +106,24 @@ def get_results(question):
             node_label="Document",
             text_node_properties=["text"],
             embedding_node_property=node_property_name,
-            retrieval_query= retrieval_query,
+            retrieval_query=retrieval_query,
         )
 
     retriever = store.as_retriever()
+
     chain = RetrievalQAWithSourcesChain.from_chain_type(
         ChatOpenAI(temperature=0), chain_type="stuff", retriever=retriever
     )
 
+    # print(question)
+    # print(PROMPT)
+    # TODO: We're not stuffing the prompt with the context documents.
+    # Should we send the prompt separately? or put the question into it and send that instead?
+
     result = chain.invoke({
         "question": question},
-        return_only_outputs = True
+        prompt=PROMPT,
+        return_only_outputs = True,
     )
 
     print(f'result: {result}')
