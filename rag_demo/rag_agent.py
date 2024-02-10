0dcfcf343ca6125bb2e1b0507a72a35a66076062
Jason Koo <jalakoo@gmail.com>
2024-02-09 22:07:24 -0800

    Unused dependencies removed

--- rag_demo/rag_agent.py
+++ rag_demo/rag_agent.py
@@ -1,10 +1,6 @@
-from langchain.agents import load_tools, AgentType, initialize_agent, create_openai_tools_agent,AgentExecutor, create_react_agent
+from langchain.agents import load_tools, AgentExecutor, create_react_agent
 from langchain import hub # requires langchainhub package
-# from langchain_community.llms import OpenAI
 from langchain_openai import OpenAI
-from langchain_experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
-from langchain_community.chat_models import ChatOpenAI
-from langchain.prompts.prompt import PromptTemplate
 from rag_demo.tools.graph_tool import graph_tool
 from rag_demo.tools.vector_tool import vector_tool
 from retry import retry
@@ -14,25 +10,8 @@ llm = OpenAI(temperature=0)
 tools = load_tools([], llm=llm)
 tools = tools + [graph_tool, vector_tool]
 
-# OLDER AGENT - Will be deprecated in future, but works with the Streamlit callback handler
-# agent_executor = initialize_agent(
-#     tools, 
-#     llm, 
-#     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, 
-#     verbose=True
-# )
 
-# agent_executor.agent.llm_chain.prompt.template
-
-# EXPERIMENTAL PLANNER
-# This goes through a very verbose chain of thought
-# model = ChatOpenAI(model="gpt-4",temperature=0)
-# planner = load_chat_planner(model)
-# planner.llm_chain.prompt.messages[0].content
-# executor = load_agent_executor(model, tools, verbose=True)
-# agent_executor = PlanAndExecute(planner=planner, executor=executor, verbose=True)
-
-# REACT AGENT EXECUTOR - Streamlit handler doesn't seem to work with this option
+# REACT AGENT EXECUTOR
 
 prompt = hub.pull("hwchase17/react")
 
@@ -49,7 +28,7 @@ agent_executor = AgentExecutor(
     # early_stopping_method = "generate"
 )
 
-@retry(tries=2, delay=12)
+@retry(tries=2, delay=6)
 def get_results(question, callbacks) -> dict:
     """Starts a LangChain agent to generate an answer using one of several Neo4j RAG tools.
 
