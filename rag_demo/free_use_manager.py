a3f5db397dbb9cc3667ccdfbbcb8cc913ec85dfa
jalakoo <jalakoo@gmail.com>
2024-03-03 22:14:58 -0800

    Default free use count set to 3 if none provided in secrets

--- rag_demo/free_use_manager.py
+++ rag_demo/free_use_manager.py
@@ -2,9 +2,16 @@
 
 import streamlit as st
 
-def free_questions_exhausted()-> bool:
+def _setup_free_questions_count():
     if "FREE_QUESTIONS_REMAINING" not in st.session_state:
-        st.session_state["FREE_QUESTIONS_REMAINING"] = st.secrets["FREE_QUESTIONS_PER_SESSION"]
+        try:
+            st.session_state["FREE_QUESTIONS_REMAINING"] = st.secrets["FREE_QUESTIONS_PER_SESSION"]
+        except:
+            st.session_state["FREE_QUESTIONS_REMAINING"] = 3
+
+def free_questions_exhausted()-> bool:
+
+    _setup_free_questions_count()
     
     remaining = st.session_state["FREE_QUESTIONS_REMAINING"]
     return remaining <= 0
@@ -18,8 +25,9 @@ def user_supplied_openai_key_unavailable()-> bool:
     return False
 
 def decrement_free_questions():
-    if "FREE_QUESTIONS_REMAINING" not in st.session_state:
-        st.session_state["FREE_QUESTIONS_REMAINING"] = st.secrets["FREE_QUESTIONS_PER_SESSION"]
+    
+    _setup_free_questions_count()
+
     remaining = st.session_state["FREE_QUESTIONS_REMAINING"]
     if remaining > 0:
         st.session_state["FREE_QUESTIONS_REMAINING"] = remaining - 1
